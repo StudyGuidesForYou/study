9c847b7288e71160742a5bfc9f4ce2ca1ce8c5b2
StudyGuidesForYou <very.cool.email2001@gmail.com>
2025-10-29 10:49:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Adjust to your frontend's dev server
+    origin: "http://localhost:5173", // Make sure this matches your frontend dev URL
     methods: ["GET", "POST"]
   }
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  let displayName = "Anonymous";
+
+  socket.on('setDisplayName', (name) => {
+    displayName = name || "Anonymous";
+  });
+
+  socket.on('message', (msg) => {
+    io.emit('message', {
+      text: msg,
+      sender: displayName,
+      timestamp: new Date(),
+    });
   });
 });
 
 server.listen(5000, () => {
-  console.log('Server is running on port 5000');
+  console.log('Server listening on port 5000');
 });
