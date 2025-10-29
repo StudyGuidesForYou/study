376eb842f911ef2f8386300aff3a24c6fa789869
StudyGuidesForYou <very.cool.email2001@gmail.com>
2025-10-29 10:44:07 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // Adjust to your frontend's dev server
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
